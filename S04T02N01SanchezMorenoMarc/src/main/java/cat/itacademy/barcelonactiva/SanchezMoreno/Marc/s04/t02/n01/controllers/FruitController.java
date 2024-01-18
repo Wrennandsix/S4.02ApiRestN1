a085b76448aac716aeca67119510671efb395d4e
Wrennandsix <marcsmoreno@hotmail.com>
2024-01-18 13:17:34 +0100

    upload

---
+++ S04T02N01SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t02/n01/controllers/FruitController.java
@@ -0,0 +1,88 @@
+package cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n01.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n01.modelDomain.Fruit;
+import cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n01.modelServices.FruitServices;
+
+@RestController
+public class FruitController {
+
+	@Autowired
+	private FruitServices fruitService;
+
+	@PostMapping("/fruita/add")
+	public ResponseEntity<String> addFruit(@RequestBody Fruit fruit) {
+		fruitService.addFruit(fruit);		
+		return new ResponseEntity<>(fruit.toString()+" succefully created in our database", HttpStatus.OK);
+	}
+
+	@PutMapping("/fruita/update/{id}")
+	public ResponseEntity<String> updateFruit(@PathVariable int id, @RequestBody Fruit newFruit) {
+		Optional<Fruit> presentFruit = fruitService.getFruitById(id);
+
+		if (presentFruit.isPresent()) {
+			Fruit updatedFruit = presentFruit.get();
+			updatedFruit.setName(newFruit.getName());
+			updatedFruit.setQuantityKg(newFruit.getQuantityKg());
+
+			fruitService.updateFruit(updatedFruit);
+
+			return new ResponseEntity<>("fruit succefully updated, "+updatedFruit.toString(), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>("fruit with id:"+id+", does not exist in our database",HttpStatus.OK);
+		}
+
+	}
+
+	@DeleteMapping("/fruita/delete/{id}")
+	public ResponseEntity<String> deleteFruit(@PathVariable int id) {
+		String fruit;
+		fruit = fruitService.getFruitById(id).toString();
+		fruit.substring(8);
+		fruitService.deleteFruit(id);
+
+		return new ResponseEntity<>(fruit+"succefully deleted from our database",HttpStatus.OK);
+
+	}
+
+	@GetMapping("/fruita/getOne/{id}")
+	public ResponseEntity<Fruit> getFruitById(@PathVariable int id) {
+
+		Optional<Fruit> fruitFinded = fruitService.getFruitById(id);
+
+		if (fruitFinded.isPresent()) {
+			return new ResponseEntity<>(fruitFinded.get(), HttpStatus.OK);
+
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@GetMapping("/fruita/getAll")
+	public ResponseEntity<List<Fruit>> getAllFruit() {
+
+		List<Fruit> fruitList = new ArrayList<>();
+		fruitService.getAllFruit().forEach(fruitList::add);
+
+		if (fruitList.isEmpty()) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(fruitList, HttpStatus.OK);
+
+		}
+
+	}
+}
---
+++ S04T02N01SanchezMorenoMarc/src/main/java/cat/itacademy/barcelonactiva/SanchezMoreno/Marc/s04/t02/n01/modelDomain/Fruit.java
@@ -0,0 +1,45 @@
+package cat.itacademy.barcelonactiva.SanchezMoreno.Marc.s04.t02.n01.modelDomain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="fruits")
+public class Fruit {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private int quantityKg;
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getQuantityKg() {
+		return quantityKg;
+	}
+
+	public void setQuantityKg(int quantityKg) {
+		this.quantityKg = quantityKg;
+	}
+	
+	@Override
+	public String toString() {
+		return "Fruit [id:" + id + ", name:" + name + ", quantityKg:" + quantityKg + "]";
+	}
+
+}
